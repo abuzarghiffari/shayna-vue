0b03b4f39a98e1a68629c1b1c31243db83a648a4
abuzarghf <rubiesolong14@gmail.com>
2021-07-26 20:10:18 +0700

    front-vue

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna }
+};
+</script>
